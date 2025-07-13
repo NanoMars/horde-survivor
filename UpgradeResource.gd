25dcd64936f691cf01e3165981c0bdcf5e619208
Armand <174164929+NanoMars@users.noreply.github.com>
2025-07-13 16:16:23 -0700

    Initial commit

---
+++ UpgradeResource.gd
@@ -0,0 +1,12 @@
+# UpgradeResource.gd
+@tool
+extends Resource
+class_name UpgradeResource
+@export var id: String               # "multi_shot"
+@export var display_name: String
+@export var description: String
+@export_range(1, 10) var max_level := 3
+
+func apply(weapon: Node, level: int) -> void:
+	# override per upgrade
+	pass
